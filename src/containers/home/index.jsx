68abd79319d2c0d5d53ab47cde6d94e44498ca9a
sahnivanika <141622433+sahnivanika@users.noreply.github.com>
2024-07-23 04:26:39 +0530

    v

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -4,6 +4,10 @@ import './style.css'; // Ensure you include the CSS file for styling
 const Home = () => {
   return (
     <div className="home-container">
+     <div className="image-container">
+     <img src={`${process.env.PUBLIC_URL}/img 1.png`} alt="Description" className="home-image" />
+
+      </div>
       <h2 className="heading">Launch Your Overseas Education Journey: Get Started with Free Consultation</h2>
       <p className="subheading">Dream, Explore, Achieve: Your Journey to Global Education Begins with Us</p>
     </div>
