6307b383b8f18ca85191c2546fa0673a356681a0
sahnivanika <141622433+sahnivanika@users.noreply.github.com>
2024-07-23 03:28:00 +0530

    q

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Home from './containers/home';
 import About from './containers/about';
 import Exams from './containers/exams';
 import Contact from './containers/contact';
+import Navbar from './components/navBar';
 
 function App() {
   return (
     <div className="App">
+    <Navbar/>
     <Routes>
-      <Route path ='/' element ={<Home/>}/>
+      <Route path ='/home' element ={<Home/>}/>
       <Route path ='/about' element={<About/>}/>
       <Route path ='/exams' element={<Exams/>}/>
       <Route path ='/contact' element={<Contact/>}/>
---
+++ src/components/navBar/index.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { FaBars, FaReact } from "react-icons/fa";
+import { HiX } from "react-icons/hi";
+import { Link } from "react-router-dom";
+import "./styles.scss";
+const data = [
+  {
+    label: "HOME",
+    to: "/",
+  },
+  {
+    label: "ABOUT ME",
+    to: "/about",
+  },
+  {
+    label: "SKILLS",
+    to: "/skills",
+  },
+  
+  {
+    label: "CONTACT",
+    to: "/contact",
+  },
+];
+
+const Navbar = () => {
+  const [toggleIcon, setToggleIcon] = useState(false);
+
+  const handleToggleIcon = () => {
+    setToggleIcon(!toggleIcon);
+  };
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar_container">
+          <Link to={"/"} className="navbar_container_logo">
+            <FaReact size={30} />
+          </Link>
+        </div>
+        <ul
+          className={`navbar_container_menu ${toggleIcon ? "active" : ""} `}
+        >
+          {data.map((item, key) => (
+            <li key={key} className="navbar_container_menu_item">
+              <Link
+                className="navbar_container_menu_item_links"
+                to={item.to}
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className="nav-icon" onClick={handleToggleIcon}>
+          {toggleIcon ? <HiX size={30} /> : <FaBars size={30} />}
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
